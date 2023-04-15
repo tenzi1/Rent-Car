a73eeb00328d97785fbe352ae46ca3aea1ecfdb7
Tshering Sherpa <sherpatshering783@gmail.com>
2023-04-15 11:11:42 +0545

    created Car model

---
+++ rental/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='car_images')
+    description = models.TextField()
+    daily_rent = models.IntegerField()
+    is_available = models.BooleanField()
+    
+    # def get_absolute_url(self):
+    #     return reverse('car-details'. kwargs={'pk':self.pk})
+
+        
